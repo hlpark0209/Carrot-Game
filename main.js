fa283a4aaeb4d8d5436b60fd15dd06334395b357
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-10 23:15:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const playBtn = document.querySelector('.play');
+const timerBtn = document.querySelector('.timer');
+const counterBtn = document.querySelector('.counter');
+
+// playBtn을 클릭했을때
+// 1. 랜덤으로 벌레와 당근 배치
+// 2. 카운트다운 시작
+// 3. 남은 당근갯수 10부터 시작
+
+
+//
